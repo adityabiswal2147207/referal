855de247a7cd8a76fd1c24768adf4757b191caf8
adityabiswal2147207 <aditya.biswal@mca.christuniversity.in>
2022-04-10 10:39:56 +0530

    christmerch half project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PaymentComponent } from './pages/payment/payment.component';
+import { PlansComponent } from './pages/plans/plans.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'about',component:AboutComponent},
+  {path:'contact',component:ContactComponent},
+  {path:'plans',component:PlansComponent},
+  {path:'payment',component:PaymentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
